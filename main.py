f979b8a5229fff328edd06c1d1600b2b9120f070
Mouaadh71 <122405864+Mouaadh71@users.noreply.github.com>
2024-09-15 23:40:25 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import pyautogui as p
+import random
+import time
+
+timer = input("how many repeat (enter f if you want to run the code forever): ")
+run_time = input("do want the time in minutes(m) or seconds(s) or miliseconds(ms): ")
+while True :
+    the_time = input(f"how many {run_time} betwen clicks :")
+    if the_time.isdigit() :
+        the_time = int(the_time)
+        break
+    else :
+        print("please entar a number")
+
+if timer == "repeat" :
+    timer = int(timer)
+    for i in range(timer):
+        if run_time == "m":
+            the_time = the_time * 60
+            p.click()
+            time.sleep(the_time)
+        elif run_time == "s":
+            p.click()
+            time.sleep(the_time)
+        elif run_time == "ms" :
+            the_time = the_time /1000
+            p.click()
+            time.sleep(the_time)
+else :
+    while True :
+        if run_time == "m":
+            the_time = the_time * 60
+            p.click()
+            time.sleep(the_time)
+        elif run_time == "s" :
+            p.click()
+            time.sleep(the_time)
+        elif run_time == "ms" :
+            the_time = the_time /1000
+            p.click()
+            time.sleep(the_time)
